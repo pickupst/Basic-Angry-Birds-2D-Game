3858f1b23e1825da7c9fb4fc49a4bedf7e882d35
pickupst <gd.okanuslu@gmail.com>
2019-01-29 12:39:45 +0300

    Add ball boundary

---
+++ Assets/Scripts/Restarter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restarter : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public float resetSpeed = 0.5f;
+
+
+    SpringJoint2D spring;
+    float resetSpeedSQR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spring = projectile.GetComponent<SpringJoint2D>();
+        resetSpeedSQR = resetSpeed * resetSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (spring == null && projectile.velocity.sqrMagnitude <= resetSpeedSQR)
+        {
+            Reset();
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() == projectile)
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
